4fdcd461284805ccfb0edbc7c3767ca0b8d581ac
Guillaume Lamaze <lamazeguillaume@hotmail.fr>
2021-08-26 14:44:40 +0200

    images

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <v-toolbar class="d-flex justify-space-between">
+      <p class="red--text darken-4 ">Toolbar</p>
+      <v-img
+        :src="require('../assets/icon.png')"
+        class=""
+        contain
+        height="30"
+      />
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
