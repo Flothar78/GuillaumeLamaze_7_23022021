57f6d5bfcd7424d55db98086a513de7a14d7cc87
Guillaume Lamaze <lamazeguillaume@hotmail.fr>
2021-08-26 21:10:52 +0200

    corrections css

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,9 @@
 <template>
   <nav>
-    <v-toolbar class="d-flex justify-space-between indigo lighten-4">
-      <p class="red--text">Toolbar</p>
-      <v-img
-        :src="require('../assets/icon.png')"
-        class=""
-        contain
-        height="30"
-      />
+    <v-toolbar class="indigo lighten-4">
+      <p class="red--text mt-3">Groupomania</p>
+      <v-spacer></v-spacer>
+      <v-img :src="require('../assets/icon.png')" max-width="30px" />
     </v-toolbar>
   </nav>
 </template>
