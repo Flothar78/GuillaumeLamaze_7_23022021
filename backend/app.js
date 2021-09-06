88f1119c2840649b451de27319fae8d6bd4460e2
Guillaume Lamaze <lamazeguillaume@hotmail.fr>
2021-09-06 12:33:43 +0200

    Signup User frontend  ==> db = ok

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-
 const path = require("path");
+
 const userRoutes = require("./routes/user");
 const userMessages = require("./routes/message");
 
@@ -17,6 +17,19 @@ try {
 } catch (error) {
   console.error("Impossible de se connecter, erreur suivante :", error);
 }
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 app.use(express.json());
 
