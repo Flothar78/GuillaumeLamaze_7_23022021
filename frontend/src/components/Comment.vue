c604a7bb173bc5b6e238bde396eb424dd86c9f87
Guillaume Lamaze <lamazeguillaume@hotmail.fr>
2021-09-15 11:30:38 +0200

    commentaire en cours affichage content OK

--- frontend/src/components/Comment.vue
+++ frontend/src/components/Comment.vue
@@ -1,50 +1,63 @@
 <template>
-  <container name:>
-    <div id="comments">Commentaires affichage  
-     <div v-for="comment in comments" :key="comment">
-     {{ comment }}
-    </div>
+  <div>
+    <div id="comments">
+      <div v-for="comment in comments" :key="comment">
+        {{ comment.content }}
+      </div>
     </div>
     <div class="d-flex">
-      <textarea v-bind:style="styleResize" placeholder="Ajouter un commentaire ?" v-model="comment"></textarea>
+      <textarea
+        v-bind:style="styleResize"
+        placeholder="Ajouter un commentaire ?"
+        v-model="comment"
+      ></textarea>
       <v-spacer></v-spacer>
       <button class="px-2 indigo lighten-4 mr-4" @click="sendComment">
         Envoyer votre commentaire
       </button>
     </div>
-  </container>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
   name: "Comment",
-  props:["messageId"],
+  props: ["messageId"],
   data() {
     return {
-    comments: [],
+      comments: [],
       comment: "",
       styleResize: {
-        resize: "both"
-      }
+        resize: "both",
+      },
     };
   },
   emits: ["comment"],
 
   async created() {
-    this.comments = await axios.get("http://localhost:3000/comments", 
-    )},
+    this.comments = await axios
+      .get("http://localhost:3000/comments")
+      .then((this.comments = (res) => res.data));
+  },
 
   methods: {
     sendComment() {
-   this.$emit("comment", this.comment);
-      axios.post("http://localhost:3000/comments", {content: this.comment, messageId:this.messageId}, {
-        headers: { Authorization: "Bearer" + " " + this.$store.state.token },
-      }).then((comments) => {console.log(comments)});
+      this.$emit("comment", this.comment);
+      axios
+        .post(
+          "http://localhost:3000/comments",
+          { content: this.comment, messageId: this.messageId },
+          {
+            headers: {
+              Authorization: "Bearer" + " " + this.$store.state.token,
+            },
+          }
+        )
+        .then((comments) => {
+          console.log(comments);
+        });
     },
-    
   },
 };
 </script>
-
-
